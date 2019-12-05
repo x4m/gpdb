0c7bbfc9cdc056b595efbb8e25a96e135e8713a4
Shivram Mani <shivram.mani@gmail.com>
2019-12-05 14:53:15 -0800

    gp_create_restore_point returns SRF with segid,lsn
    Updated the catalog function gp_create_restore_point to capture the segment id
    along with the restore point lsn for master and each of the segments. Returns
    SETOF RECORD with fields segment_id and restore_lsn.
    
    Note: Running the function as part of INSERT INTO SELECT * would result in a
    failure due to double dispatch. The query planner has the function running on a
    QE segment but the function should only be run from the QD.
    
    Co-authored-by: Jimmy Yih <jyih@pivotal.io>

--- src/include/catalog/pg_proc_gp.h
+++ src/include/catalog/pg_proc_gp.h
@@ -983,8 +983,8 @@ DESCR("Legacy cdbhash function");
 DATA(insert OID = 6171 ( cdblegacyhash_anyenum  PGNSP PGUID 12 1 0 0 0 f f f f t f i 1 0 23 "3500" _null_ _null_ _null_ _null_ cdblegacyhash_anyenum _null_ _null_ _null_ n a ));
 DESCR("Legacy cdbhash function");
 
-/* gp_create_restore_point(text) => bool */
-DATA(insert OID = 6998 ( gp_create_restore_point  PGNSP PGUID 12 1 0 0 0 f f f f t f i 1 0 16 "25" _null_ _null_ _null_ _null_ gp_create_restore_point _null_ _null_ _null_ n a ));
+/* gp_create_restore_point(text) => SETOF record */
+DATA(insert OID = 6998 ( gp_create_restore_point  PGNSP PGUID 12 1 1000 0 0 f f f f t t i 1 0 2249 "25" _null_ _null_ _null_ _null_ gp_create_restore_point _null_ _null_ _null_ n a ));
 DESCR("Create a named restore point on all segments");
 
 
